efa4012d99e2038e5372cdf5c50a05ace80ad7d5
Nick <zhongcheng509@gmail.com>
2019-10-03 16:40:08 +0800

    验证iconPosition为left或right中的一个

--- src/g-button.vue
+++ src/g-button.vue
@@ -12,7 +12,16 @@
 <script>
   export default {
     name: 'g-button',
-    props: ['icon', 'iconPosition']
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator (value) {
+          return value === 'left' || value === 'right'
+        }
+      }
+    }
   }
 </script>
 
