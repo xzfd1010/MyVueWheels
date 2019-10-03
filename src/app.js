fba75d9720e4537b5f24a9de087256052b28b456
Nick <zhongcheng509@gmail.com>
2019-10-03 17:24:13 +0800

    添加button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import GButton from './g-button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('my-button', GButton)
 Vue.component('icon', Icon)
+Vue.component('my-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
