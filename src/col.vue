9a9135a99ed6114f6d3064c521385e275815d956
Nick <zhongcheng509@gmail.com>
2019-10-18 09:26:05 +0800

    重构col和row的代码

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2+'px',paddingRight:gutter/2+'px'}">
-    <div>
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass"
+       :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -23,6 +21,22 @@
         gutter: 0
       }
     },
+    computed: {
+      colClass () {
+        let { span, offset } = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle () {
+        let { gutter } = this
+        return {
+          paddingLeft: gutter / 2 + 'px',
+          paddingRight: gutter / 2 + 'px'
+        }
+      }
+    }
   }
 </script>
 
