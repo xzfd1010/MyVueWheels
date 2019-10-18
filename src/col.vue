84bede2bc8ecb425fe1632df60f0e1cb41cfd1a4
Nick <zhongcheng509@gmail.com>
2019-10-18 12:00:57 +0800

    重构生成col class的部分

--- src/col.vue
+++ src/col.vue
@@ -35,16 +35,28 @@
         gutter: 0
       }
     },
+    methods: {
+      createClass (spanAndOffset, prefix = '') {
+        if (!spanAndOffset) return []
+        let classes = []
+        if (spanAndOffset.span) {
+          classes.push(`col-${prefix}${spanAndOffset.span}`)
+        }
+        if (spanAndOffset.offset) {
+          classes.push(`col-${prefix}${spanAndOffset.offset}`)
+        }
+        return classes
+      }
+    },
     computed: {
       colClass () {
-        let { span, offset, ipad, narrowPc, pc, widePc } = this
+        let { span, offset, ipad, narrowPc, pc, widePc, createClass } = this
         return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`,
-          ...[ipad ? `col-ipad-${ipad.span}` : []],
-          ...[narrowPc ? `col-narrow-pc-${narrowPc.span}` : []],
-          ...[pc ? `col-pc-${pc.span}` : []],
-          ...[widePc ? `col-wide-pc-${widePc.span}` : []],
+          createClass({ span, offset }),
+          createClass(ipad, 'ipad-'),
+          createClass(narrowPc, 'narrow-pc-'),
+          createClass(pc, 'pc-'),
+          createClass(widePc, 'wide-pc-')
         ]
       },
       colStyle () {
@@ -77,7 +89,7 @@
         margin-left: $n / 24 * 100%;
       }
     }
-    @media(min-width: 569px){
+    @media(min-width: 569px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -91,7 +103,7 @@
         }
       }
     }
-    @media(min-width: 769px){
+    @media(min-width: 769px) {
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -105,7 +117,7 @@
         }
       }
     }
-    @media(min-width: 993px){
+    @media(min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
