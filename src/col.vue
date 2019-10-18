306b39d1e796f15ae90d5a963c8780a5c27f9367
Nick <zhongcheng509@gmail.com>
2019-10-18 11:57:12 +0800

    添加不同设备尺寸的判断

--- src/col.vue
+++ src/col.vue
@@ -25,7 +25,6 @@
       offset: {
         type: [Number, String]
       },
-      phone: { type: Object, validator },
       ipad: { type: Object, validator },
       narrowPc: { type: Object, validator },
       pc: { type: Object, validator },
@@ -38,11 +37,10 @@
     },
     computed: {
       colClass () {
-        let { span, offset, phone, ipad, narrowPc, pc, widePc } = this
+        let { span, offset, ipad, narrowPc, pc, widePc } = this
         return [
           span && `col-${span}`,
           offset && `offset-${offset}`,
-          ...[phone ? `col-phone-${phone.span}` : []],
           ...[ipad ? `col-ipad-${ipad.span}` : []],
           ...[narrowPc ? `col-narrow-pc-${narrowPc.span}` : []],
           ...[pc ? `col-pc-${pc.span}` : []],
@@ -79,21 +77,7 @@
         margin-left: $n / 24 * 100%;
       }
     }
-    @media(max-width: 568px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: $n / 24 * 100%;
-        }
-      }
-      $offset-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$offset-prefix}#{$n} {
-          margin-left: $n / 24 * 100%;
-        }
-      }
-    }
-    @media(min-width: 569px) and(max-width: 768px) {
+    @media(min-width: 569px){
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -107,7 +91,7 @@
         }
       }
     }
-    @media(min-width: 769px) and(max-width: 992px) {
+    @media(min-width: 769px){
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -121,7 +105,7 @@
         }
       }
     }
-    @media(min-width: 993px) and(max-width: 1200px) {
+    @media(min-width: 993px){
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
