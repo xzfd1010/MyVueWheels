2a001b7f993bd302dea8abab61607f69b766d137
Nick <zhongcheng509@gmail.com>
2019-10-18 09:13:09 +0800

    基本实现col和row的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+       :style="{paddingLeft: gutter/2+'px',paddingRight:gutter/2+'px'}">
+    <div>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,8 +13,16 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      },
+    },
+    data () {
+      return {
+        gutter: 0
       }
-    }
+    },
   }
 </script>
 
@@ -19,12 +30,20 @@
   .col {
     width: 50%;
     height: 50px;
-    background: gray;
-    border: 1px solid red;
+    > div {
+      background: gray;
+      border: 1px solid red;
+    }
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: $n / 24 * 100%;
+      }
+    }
+    $offset-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$offset-prefix}#{$n} {
+        margin-left: $n / 24 * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'my-row'
+    name: 'my-row',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    mounted () {
+      // 挂载的时候能够拿到$children
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+      })
+    }
   }
 </script>
 
