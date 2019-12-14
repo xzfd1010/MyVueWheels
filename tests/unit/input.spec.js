309f6ff136568e5a3fce59fd3ee90e6a751ecb2f
Nick <zhongcheng509@gmail.com>
2019-12-14 17:50:25 +0800

    修改input的相关测试用例

--- tests/unit/input.spec.js
+++ tests/unit/input.spec.js
@@ -1,71 +1,69 @@
-const expect = chai.expect
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import { mount } from '@vue/test-utils'
 import Vue from 'vue'
 import Input from '@/input'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
+chai.use(sinonChai)
+
 describe('Input', () => {
   it('存在.', () => {
     expect(Input).to.exist
   })
   describe('props', () => {
-    const Constructor = Vue.extend(Input)
-    let vm
-    afterEach(() => {
-      vm.$destroy()
-    })
     it('可以接受value.', () => {
-      vm = new Constructor({
+      const wrapper = mount(Input, {
         propsData: {
           value: '张三'
         }
-      }).$mount()
-      const input = vm.$el.querySelector('input')
+      })
+      const input = wrapper.element.querySelector('input')
       expect(input.value).to.equal('张三')
     })
     it('可以接受disabled.', () => {
-      vm = new Constructor({
+      const wrapper = mount(Input, {
         propsData: {
           disabled: true
         }
-      }).$mount()
-      const input = vm.$el.querySelector('input')
+      })
+      const input = wrapper.element.querySelector('input')
       expect(input.disabled).to.equal(true)
     })
     it('可以接受readonly.', () => {
-      vm = new Constructor({
+      const wrapper = mount(Input, {
         propsData: {
           readonly: true
         }
-      }).$mount()
-      const input = vm.$el.querySelector('input')
+      })
+      const input = wrapper.element.querySelector('input')
       expect(input.readOnly).to.equal(true)
     })
     it('可以接受error.', () => {
-      vm = new Constructor({
+      const wrapper = mount(Input, {
         propsData: {
           error: '你错了'
         }
-      }).$mount()
-      const errorElement = vm.$el.querySelector('.error-message')
-      const useElement = vm.$el.querySelector('use')
+      })
+      const errorElement = wrapper.element.querySelector('.error-message')
+      const useElement = wrapper.element.querySelector('use')
       expect(useElement.getAttribute('xlink:href')).to.equal('#icon-error')
       expect(errorElement.innerText).to.equal('你错了')
     })
   })
   describe('测试事件', () => {
-    const Constructor = Vue.extend(Input)
-    let vm
-    afterEach(() => {
-      vm.$destroy()
-    });
     ['change', 'focus', 'input', 'blur'].forEach(eventName => {
       it('change/focus/input/blur事件测试', () => {
-        vm = new Constructor().$mount()
-        const inputElement = vm.$el.querySelector('input')
         const callback = sinon.fake()
-        vm.$on(eventName, callback) // 组件绑定事件
+        const wrapper = mount(Input, {
+          listeners: {
+            [eventName]: callback
+          }
+        })
+        const inputElement = wrapper.element.querySelector('input')
         const event = new Event(eventName)
         Object.defineProperty(
           event, 'target', {
